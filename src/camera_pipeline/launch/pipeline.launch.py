3da9a35d0cb73cfc5a5e7b9524412bed92b5dcd3
Oliver <olivers.gundersen@gmail.com>
2025-04-10 11:55:06 +0200

    Launch container

--- src/camera_pipeline/launch/pipeline.launch.py
+++ src/camera_pipeline/launch/pipeline.launch.py
@@ -1,14 +1,20 @@
 from launch import LaunchDescription
-from launch_ros.actions import Node
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
 
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package='image_proc',
-            executable='image_proc',  # rectify_node er en komponent i image_proc
-            name='rectify_node',
-            output='screen'
-        )
-    ])
-
-
+    container = ComposableNodeContainer(
+        name='image_proc_container',
+        namespace='',
+        package='rclcpp_components',
+        executable='component_container_mt',  # multi-threaded container, alternativt "component_container" om nødvendig
+        composable_node_descriptions=[
+            ComposableNode(
+                package='image_proc',
+                plugin='image_proc::RectifyNode',  # Plugin-navnet for rectification-noden
+                name='rectify_node'
+            )
+        ],
+        output='screen',
+    )
+    return LaunchDescription([container])
